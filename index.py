0cdc46ada935ee6c3a7db1d8132e88db8a9c72a9
geniucker <wolf2003rain@163.com>
2022-12-07 01:46:28 +0800

    debug

--- index.py
+++ index.py
@@ -3,16 +3,16 @@ from subprocess import getoutput
 from flask import Flask, request
 from urllib.parse import urlencode
 import requests
-from ping3 import ping
 
 
 app = Flask(__name__)
 @app.route("/")
 def index():
     args = request.args
-    # return requests.get("http://127.0.0.1:1234/sub?"+urlencode(args)).text, {'Content-Type': 'text/yaml;charset=utf-8'}
-    return str(ping('localhost'))
+    return requests.get("http://127.0.0.1:1234/sub?"+urlencode(args)).text, {'Content-Type': 'text/yaml;charset=utf-8'}
 
 if __name__ == "__main__":
-    system("subconverter/subconverter &")
-    app.run(host="0.0.0.0", port=221, debug=True)
+    system("sh start.sh")
+    # app.run(host="0.0.0.0", port=221, debug=False)
+    server = pywsgi.WSGIServer(('0.0.0.0', 221), app)
+    server.serve_forever()
