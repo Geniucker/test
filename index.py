d4149121d6b4178efb10cf91c1d2d6d367f1a0ca
geniucker <wolf2003rain@163.com>
2022-12-07 01:56:45 +0800

    debug

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ import requests
 
 
 app = Flask(__name__)
-@app.route("/")
+@app.route("/sub")
 def index():
     args = request.args
     return requests.get("http://127.0.0.1:1234/sub?"+urlencode(args)).text, {'Content-Type': 'text/yaml;charset=utf-8'}
@@ -14,5 +14,5 @@ def index():
 if __name__ == "__main__":
     system("sh start.sh")
     # app.run(host="0.0.0.0", port=221, debug=False)
-    server = pywsgi.WSGIServer(('0.0.0.0', 221), app)
+    server = pywsgi.WSGIServer(('0.0.0.0', 224), app)
     server.serve_forever()
