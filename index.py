c522f868be548ed36d3ecd8ba1bd27242aea51b9
geniucker <wolf2003rain@163.com>
2022-12-07 01:22:08 +0800

    fix

--- index.py
+++ index.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     args = request.args
-    return requests.get("http://127.0.0.1:1234/sub?"+urlencode(args)).text, {'Content-Type': 'text/yaml;charset=utf-8'}
-
+    # return requests.get("http://127.0.0.1:1234/sub?"+urlencode(args)).text, {'Content-Type': 'text/yaml;charset=utf-8'}
+    return getoutput("uname -a")
 
 if __name__ == "__main__":
     system("subconverter/subconverter &")
