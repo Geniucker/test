3903e24d4bb2dad76617f2465619a49fab1bb698
geniucker <wolf2003rain@163.com>
2022-12-07 01:35:26 +0800

    debut

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def index():
     args = request.args
     # return requests.get("http://127.0.0.1:1234/sub?"+urlencode(args)).text, {'Content-Type': 'text/yaml;charset=utf-8'}
-    return ping('localhost')
+    return str(ping('localhost'))
 
 if __name__ == "__main__":
     system("subconverter/subconverter &")
