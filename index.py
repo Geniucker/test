f141727d80659b43fce7376d1ae831b8006198c4
geniucker <wolf2003rain@163.com>
2022-12-07 01:00:46 +0800

    debut

--- index.py
+++ index.py
@@ -3,12 +3,12 @@ from subprocess import getoutput
 from flask import Flask
 
 
-app = Flask(__name__)
-@app.route("/")
-def index():
-    system("nohup subconverter/subconverter")
-    return getoutput("subconverter/subconverter")
+# app = Flask(__name__)
+# @app.route("/")
+# def index():
+system("nohup subconverter/subconverter")
+#    return getoutput("subconverter/subconverter")
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=221, debug=True)
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=221, debug=True)
