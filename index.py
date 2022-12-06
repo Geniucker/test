11b9baa7d4ab56802eec311a55422833869a173b
geniucker <wolf2003rain@163.com>
2022-12-07 01:25:54 +0800

    fix

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def index():
     args = request.args
     # return requests.get("http://127.0.0.1:1234/sub?"+urlencode(args)).text, {'Content-Type': 'text/yaml;charset=utf-8'}
-    return getoutput("uname -a")
+    return getoutput("ifconfig")
 
 if __name__ == "__main__":
     system("subconverter/subconverter &")
